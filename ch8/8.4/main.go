4246b39cfecb9eabea95d0d6eef3224fc5d25872
Skip Gibson <skipcloudgibson@gmail.com>
2020-04-28 08:46:54 +0100

    ch8: finished 8.4

---
+++ ch8/8.4/main.go
@@ -0,0 +1,63 @@
+// Reverb2 is a TCP server that simulates an echo.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+/*
+	Modify the reverb2  server to use a sync.WaitGroup per connection to count
+	the number of active echo goroutines. When it falls to zero, close the write
+	half of the TCP connection as described in Exercise 8.3. Verify that your
+	modified netcat3 client from that exercise waits for the final echoes of
+	multiple concurrent shouts, even after the standard input has been closed
+*/
+
+func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
+	for input.Scan() {
+		wg.Add(1)
+		// We need to pass a pointer to the wait group, otherwise
+		// it is passed by value and wg.Done() in the function won't
+		// affect the wait group in this scope as it is a different
+		// object
+		go echo(c, input.Text(), 1*time.Second, &wg)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	fmt.Println("waiting")
+	wg.Wait()
+	fmt.Println("done waiting, closing conn")
+	conn := c.(*net.TCPConn)
+	conn.CloseWrite()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
